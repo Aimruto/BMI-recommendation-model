cd9131f8da0e13663f68b7c4f63ef430520c8ccd
Aimruto <96933091+Aimruto@users.noreply.github.com>
2024-04-03 20:01:08 +0530

    Update BmiModel.py

--- model_trained/BmiModel.py
+++ model_trained/BmiModel.py
@@ -3,45 +3,36 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 
-# Load data
+
 data = pd.read_csv('C:/Users/KIIT/Documents/new-minor/Data/input.csv')
 
-# Label encode the gender column
 lbl_enc = LabelEncoder()
 data.iloc[:, 0] = lbl_enc.fit_transform(data.iloc[:, 0])
 
-# Standardize features except the last column
+
 std_sc = StandardScaler()
 df = pd.DataFrame(data)
 df.iloc[:, 1:-1] = std_sc.fit_transform(df.iloc[:, 1:-1])
 
-# Split data into features and target
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 
-# Train RandomForestClassifier
 rfc = RandomForestClassifier(n_estimators=200, criterion='entropy', random_state=0)
 rfc.fit(X, y)
 
-# Load recommendations from CSV file
 recommendations_df = pd.read_csv('C:/Users/KIIT/Documents/new-minor/Data/recom.csv')
 
-# Define function to predict health status and provide recommendations
 def health_test(gender, height, weight):
     '''Input gender as Male/Female, height in cm, weight in Kg'''
     individual_data_dict = {'Gender': [gender], 'Height': [height], 'Weight': [weight]}
     individual_data = pd.DataFrame(data=individual_data_dict)
 
-    # Label encode the gender column
     individual_data.iloc[:, 0] = lbl_enc.transform(individual_data.iloc[:, 0])
 
-    # Standardize features
     individual_data.iloc[:, 1:] = std_sc.transform(individual_data.iloc[:, 1:])
 
-    # Predict the health status
     y_pred = rfc.predict(individual_data)
 
-    # Map predicted labels to corresponding categories
     label_mapping = {
         0: 'Extremely Weak',
         1: 'Weak',
@@ -52,19 +43,17 @@ def health_test(gender, height, weight):
     }
     health_status = label_mapping[y_pred[0]]
 
-    # Fetch recommendations from CSV file based on health status and gender
     recommendations = get_recommendations(health_status, gender)
 
     return health_status, recommendations
 
-# Define function to fetch recommendations based on health status and gender
 def get_recommendations(health_status, gender):
     recommendations = recommendations_df[(recommendations_df['Health Category'] == health_status) & 
                                           (recommendations_df['Gender'] == gender)]
     exercise_recommendation = recommendations['Exercise Recommendation'].iloc[0]
     diet_recommendation = recommendations['Diet Recommendation'].iloc[0]
     return exercise_recommendation, diet_recommendation
-# Change the tab title
+
 st.set_page_config(page_title="BMI Health Recommendation")
 
 # Streamlit UI
